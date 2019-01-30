964ded2d70d7bbbdc876fc7ddb170523a93c5744
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-30 12:00:28 -0500

    adding .gitignore

---
+++ Git Collaboration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Collaboration
+//
+//  Created by Kamal Maged on 2019-01-30.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
