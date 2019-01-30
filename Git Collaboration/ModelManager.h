c5e60d037e7d4580ed50d98618eef3f57e2d7728
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-30 12:25:13 -0500

    adding color code

---
+++ Git Collaboration/ModelManager.h
@@ -0,0 +1,17 @@
+//
+//  ModelManager.h
+//  Git Collaboration
+//
+//  Created by Kamal Maged on 2019-01-30.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ModelManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Git Collaboration/ModelManager.m
@@ -0,0 +1,13 @@
+//
+//  ModelManager.m
+//  Git Collaboration
+//
+//  Created by Kamal Maged on 2019-01-30.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import "ModelManager.h"
+
+@implementation ModelManager
+
+@end
